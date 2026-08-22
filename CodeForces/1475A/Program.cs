9edb63f961b5ce4fcd2d25cb84a46f53992b204e
ELgago73 <gandjergholi@gmail.com>
2026-08-22 22:58:40 +0400

    add CodeForces folder

---
+++ CodeForces/1475A/Program.cs
@@ -0,0 +1,10 @@
+﻿int t = int.Parse(Console.ReadLine());
+while (t-- > 0)
+{
+    long n = long.Parse(Console.ReadLine());
+    while (n % 2 == 0)
+    {
+        n /= 2;
+    }
+    Console.WriteLine((n== 1) ? "NO" : "YES");
+}
