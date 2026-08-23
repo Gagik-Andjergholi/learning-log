a43f2f0edd699e97ef761c4943c702310d9e7787
ELgago73 <gandjergholi@gmail.com>
2026-08-23 23:34:48 +0400

    CodeForces problem 339B

---
+++ CodeForces/339B/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] firstLine = Console.ReadLine().Split(' ');
+long n, m, ans = 0;
+n = long.Parse(firstLine[0]);
+m = long.Parse(firstLine[1]);
+int[] a = new[] { 1 }.Concat(Console.ReadLine().Split(' ').Select(int.Parse)).ToArray();
+for(int i = 0; i < m; i++)
+{
+    ans += (a[i] <= a[i + 1]) ? a[i + 1] - a[i] : n + a[i + 1] - a[i];
+}
+Console.WriteLine(ans);
