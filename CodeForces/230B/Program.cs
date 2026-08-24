f6d988929489a4d958b3a191b3c5467fc46a8e58
ELgago73 <gandjergholi@gmail.com>
2026-08-24 21:54:01 +0400

    CodeForces problem 230B

---
+++ CodeForces/230B/Program.cs
@@ -0,0 +1,28 @@
+﻿bool[] p = new bool[1000050];
+Array.Fill(p, true);
+p[0] = p[1] = false;
+for(int i = 2; i <= 1000040; i++)
+{
+    if (p[i])
+    {
+        for(int j = 2 * i; j < 1000040; j += i)
+        {
+            p[j] = false;
+        }
+    }
+}
+
+int n = int.Parse(Console.ReadLine());
+long[] a = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+for(int i = 0; i < n; i++)
+{
+    double sqrt = Math.Sqrt(a[i]);
+    if (Math.Floor(sqrt) == sqrt && p[(int)sqrt])
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
