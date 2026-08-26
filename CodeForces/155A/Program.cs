64a0cc0faf854d182cef05e9baa0ab38548e29a2
ELgago73 <gandjergholi@gmail.com>
2026-08-26 21:13:54 +0400

    155A

---
+++ CodeForces/155A/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int cnt = 0,max = a[0], min = a[0];
+for (int i = 1; i < n; i++)
+{
+    if(a[i] > max)
+    {
+        max = a[i];
+        cnt++;
+    }
+    else if(a[i] < min)
+    {
+        min = a[i];
+        cnt++;
+    }
+}
+Console.WriteLine(cnt);
