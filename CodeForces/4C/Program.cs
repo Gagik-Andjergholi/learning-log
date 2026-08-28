35aca3d7cf9564757499f33d3a749c46f0172a8d
ELgago73 <gandjergholi@gmail.com>
2026-08-28 21:19:29 +0400

    CodeForces problem 4C

---
+++ CodeForces/4C/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<string, int> dict = new Dictionary<string, int>();
+for (int i = 0; i < n; i++)
+{
+    string s = Console.ReadLine();
+    if (dict.ContainsKey(s))
+    {
+        dict[s]++;
+        Console.WriteLine(s + dict[s]);
+    }
+    else
+    {
+        Console.WriteLine("OK");
+        dict[s] = 0;
+    }
+}
