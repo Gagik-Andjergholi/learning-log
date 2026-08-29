cc948d74ab9c74c5a287f07eb93727535059b8bc
ELgago73 <gandjergholi@gmail.com>
2026-08-29 18:19:10 +0400

    478B

---
+++ CodeForces/478B/Program.cs
@@ -0,0 +1,8 @@
+﻿long[] a = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+long n = a[0], m = a[1], ans = 0;
+long c(long x)
+{
+    return (x - 1) * x / 2;
+}
+ans = (n % m) * c(n / m + 1) + (m - n % m) * c(n / m);
+Console.WriteLine(ans + " " + c(n - m + 1));
