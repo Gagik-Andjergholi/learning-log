474c635cef195a67490250e2651bd826c2ce909f
ELgago73 <gandjergholi@gmail.com>
2026-08-30 21:31:36 +0400

    CodeForces problem 1742A

---
+++ CodeForces/1742A/Program.cs
@@ -0,0 +1,13 @@
+﻿int t = int.Parse(Console.ReadLine());
+for(int i = 0; i < t; i++)
+{
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    if(a[0] + a[1] == a[2] || a[1] + a[2] == a[0] || a[0] + a[2] == a[1])
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
