46eba682008d628447bbaadd3f887b1216641a76
ELgago73 <gandjergholi@gmail.com>
2026-09-01 00:33:28 +0400

    Code Forces problem 208A

---
+++ CodeForces/208A/Program.cs
@@ -0,0 +1,21 @@
+﻿string s = Console.ReadLine();
+string ans = "";
+bool b = false;
+for(int i = 0; i < s.Length; i++)
+{
+    if(b && i + 3 <= s.Length && s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
+    {
+        ans += " ";
+        b = false;
+        i += 2;
+        continue;
+    }
+    if(!b && i + 3 <= s.Length && s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
+    {
+        i += 2;
+        continue;
+    }
+    ans += s[i];
+    b = true;
+}
+System.Console.WriteLine(ans);
