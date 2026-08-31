f420edd25e32a42442e94e2a7a9681f94047f23a
ELgago73 <gandjergholi@gmail.com>
2026-09-01 02:02:06 +0400

    Code Forces problem 1899A

---
+++ CodeForces/1899A/Program.cs
@@ -0,0 +1,10 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n;
+while(t-- > 0)
+{
+    n = int.Parse(Console.ReadLine());
+    if(n % 3 == 0)
+        System.Console.WriteLine("Second");
+    else
+        System.Console.WriteLine("First");
+}
