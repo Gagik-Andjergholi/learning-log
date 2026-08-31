d038432b1c15b475fe04d30dc11e18278b18f0b1
ELgago73 <gandjergholi@gmail.com>
2026-09-01 01:49:25 +0400

    Code Forces problem 520A

---
+++ CodeForces/520A/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine());
+string s = Console.ReadLine();
+s = s.ToLower();
+char[] c = s.ToCharArray();
+c = c.Distinct().ToArray();
+if(c.Length == 26)
+    System.Console.WriteLine("Yes");
+else
+    System.Console.WriteLine("NO");
