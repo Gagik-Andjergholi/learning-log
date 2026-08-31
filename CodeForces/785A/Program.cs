ef1aeefedd9a5353836775030aaff3cbc1016b65
ELgago73 <gandjergholi@gmail.com>
2026-09-01 01:55:33 +0400

    Code Forces problem 785A

---
+++ CodeForces/785A/Program.cs
@@ -0,0 +1,26 @@
+﻿int n = int.Parse(Console.ReadLine());
+int ans = 0;
+string s;
+while(n-- > 0)
+{
+    s = Console.ReadLine();
+    switch (s)
+    {
+        case "Tetrahedron":
+            ans += 4;
+            break;
+        case "Cube":
+            ans += 6;
+            break;
+        case "Octahedron":
+            ans += 8;
+            break;
+        case "Dodecahedron":
+            ans += 12;
+            break;
+        default:
+            ans += 20;
+            break;
+    }
+}
+System.Console.WriteLine(ans);
