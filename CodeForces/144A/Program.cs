9ded7770b37be4fbf09a846b1b96005f1fc0f01c
ELgago73 <gandjergholi@gmail.com>
2026-09-01 02:20:08 +0400

    Code Forces problem 144A

---
+++ CodeForces/144A/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Linq;
+int n = int.Parse(Console.ReadLine());
+int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+Console.WriteLine(Array.IndexOf(a, a.Max()) +
+             n - 1 - Array.LastIndexOf(a, a.Min()) - 
+(Array.LastIndexOf(a, a.Min()) < Array.IndexOf(a, a.Max()) ? 1 : 0));
