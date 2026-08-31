1799ad66354570a8e320592fa434bb8ba16db61c
ELgago73 <gandjergholi@gmail.com>
2026-09-01 00:19:13 +0400

    Code Forces problem 510A

---
+++ CodeForces/1703A/Program.cs
@@ -0,0 +1,10 @@
+﻿int t = int.Parse(Console.ReadLine());
+string s;
+while(t-- > 0){
+    s = Console.ReadLine();
+    if(s.ToLower() == "yes")
+    {
+        System.Console.WriteLine("YES");
+    }else
+        System.Console.WriteLine("NO");
+}
---
+++ CodeForces/510A/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+string[] s = new string[4];
+s[0] = string.Concat(Enumerable.Repeat("#", a[1]));
+s[1] = string.Concat(Enumerable.Repeat(".", a[1] - 1));
+(s[2], s[3]) = (s[0], s[1]);
+(s[1] , s[3]) = (s[1] + '#', '#' + s[3]);
+for(int i = 0; i < a[0]; i++)
+    System.Console.WriteLine(s[i % 4]);
