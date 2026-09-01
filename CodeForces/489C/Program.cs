50f4ef8c9802b5ff9d9cf151c31e60a3fa3476ad
ELgago73 <gandjergholi@gmail.com>
2026-09-01 23:28:25 +0400

    Code Forces problem 489C

---
+++ CodeForces/489C/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] goozoo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = goozoo[0], m = goozoo[1];
+int[] a = new int[n];
+if(m == 0 && n == 1) {
+    Console.WriteLine(0 + " " + 0);
+    return;
+}
+if(!(1 <= m && m <= 9 * n)) {
+    Console.WriteLine(-1 + " " + -1);
+    return;
+}
+for(int i = 0; i < n; i++) {
+    if(m >= 9) {
+        a[i] = 9;
+        m -= 9;
+    } else {
+        a[i] = m;
+        m = 0;
+    }
+}
+string s = string.Join("", a) + " ";
+Array.Reverse(a);
+if(a[0] == 0)
+{
+    for(int i = 1; i < n; i++) {
+        if(a[i] > 0) {
+            a[i]--;
+            a[0]++;
+            break;
+        }
+    }
+}
+Console.Write(string.Join("", a) + " ");
+System.Console.WriteLine(s);
