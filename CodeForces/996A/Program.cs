3413b5d41da4f165ef6ac7319c1f418745055f81
ELgago73 <gandjergholi@gmail.com>
2026-09-02 00:28:36 +0400

    996A

---
+++ CodeForces/996A/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+int ans = 0;
+ans += n / 100;
+n = n % 100;
+ans += n / 20;
+n = n % 20;
+ans += n / 10;
+n = n % 10;
+ans += n / 5;
+n = n % 5;
+ans += n;
+Console.WriteLine(ans);
