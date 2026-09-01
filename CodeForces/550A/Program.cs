57ffdcb209f0179c6977bbd7676bcba3ef915e29
ELgago73 <gandjergholi@gmail.com>
2026-09-02 01:36:06 +0400

    550A

---
+++ CodeForces/550A/Program.cs
@@ -0,0 +1,5 @@
+﻿string s = Console.ReadLine();
+int abf = s.IndexOf("AB"), bal = s.LastIndexOf("BA");
+int abl = s.LastIndexOf("AB"), baf = s.IndexOf("BA");
+System.Console.WriteLine((abf != -1 && baf != -1 && 
+    (Math.Abs(abf - bal) != 1 || Math.Abs(abl - baf) != 1) ? "YES" : "NO"));
