24e1ff6650e9ebf447a46b17e7761eeac7e7e239
ELgago73 <gandjergholi@gmail.com>
2026-09-03 23:12:49 +0400

    Code Forces problem 1903A

---
+++ CodeForces/1903A/Program.cs
@@ -0,0 +1,24 @@
+﻿int t = int.Parse(Console.ReadLine());
+while (t-- > 0)
+{
+    int[] x = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    if(x[1] != 1)
+    {
+        Console.WriteLine("YES");
+        continue;
+    }
+    for(int i = 1; i < x[0]; i++)
+    {
+        if(a[i] < a[i - 1])
+        {
+            Console.WriteLine("NO");
+            x[1] = -1;
+            break;
+        }
+    }
+    if(x[1] != -1)
+    {
+        Console.WriteLine("YES");
+    }
+}
