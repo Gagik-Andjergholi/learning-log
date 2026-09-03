e4711ef1557c9ff76374557bf374b05b06470739
ELgago73 <gandjergholi@gmail.com>
2026-09-03 22:04:30 +0400

    Code Forces problem 750A

---
+++ CodeForces/750A/Program.cs
@@ -0,0 +1,2 @@
+﻿int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+System.Console.WriteLine(Math.Min((int)Math.Floor((Math.Sqrt(1 + (double)8/5 * (240 - a[1])) - 1) / 2), a[0]));
