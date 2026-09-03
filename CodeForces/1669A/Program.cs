fb5d3f1bb99cfca9ec60f0b30ee5b5dd25652784
ELgago73 <gandjergholi@gmail.com>
2026-09-03 22:08:52 +0400

    Code Forces problem 1669A

---
+++ CodeForces/1669A/Program.cs
@@ -0,0 +1,5 @@
+﻿int t = int.Parse(Console.ReadLine());
+for(int i = 0; i < t; i++){
+    int n = int.Parse(Console.ReadLine());
+    System.Console.WriteLine(n >= 1900 ? "Division 1" : n >= 1600 ? "Division 2" : n >= 1400 ? "Division 3" : "Division 4");
+}
