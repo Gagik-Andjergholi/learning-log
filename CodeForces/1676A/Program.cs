04a908de537dd5c1d420dcd2f8402f44e7b6b37f
ELgago73 <gandjergholi@gmail.com>
2026-09-03 22:30:55 +0400

    Code Forces problem

---
+++ CodeForces/1676A/Program.cs
@@ -0,0 +1,13 @@
+﻿int t = int.Parse(Console.ReadLine());
+while(t-- > 0)
+{
+    string s = Console.ReadLine();
+    if((int)s[0] + (int)s[1] + (int)s[2] == (int)s[3] + (int)s[4] + (int)s[5])
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
