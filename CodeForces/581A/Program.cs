c70358a015091c64427802b77ec1529204a1f24e
ELgago73 <gandjergholi@gmail.com>
2026-09-04 19:59:25 +0400

    Code Forces problem 581A

---
+++ CodeForces/581A/Program.cs
@@ -0,0 +1,2 @@
+﻿int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+Console.WriteLine(Math.Min(arr[0], arr[1]) + " " + (Math.Max(arr[0], arr[1]) - Math.Min(arr[0], arr[1])) / 2);
