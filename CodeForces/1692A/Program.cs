877d7aa892d896c09bc827ab8ea976530d5ff922
ELgago73 <gandjergholi@gmail.com>
2026-09-04 19:47:51 +0400

    Code Forces problem 1692A

---
+++ CodeForces/1692A/Program.cs
@@ -0,0 +1,6 @@
+﻿int t = int.Parse(Console.ReadLine());
+while(t-- > 0)
+{
+    int[ ] a = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+    Console.WriteLine((a[0] > a[1] ? 0 : 1)  + (a[0] > a[2] ? 0 : 1) + (a[0] > a[3] ? 0 : 1));
+}
