a2d8ca77f3966871834d393e74e5f4061e00f948
ELgago73 <gandjergholi@gmail.com>
2026-09-04 19:51:58 +0400

    Code Forces problem 1807A

---
+++ CodeForces/1807A/Program.cs
@@ -0,0 +1,6 @@
+﻿int t = int.Parse(Console.ReadLine());
+while(t-- > 0)
+{
+    int[ ] a = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+    Console.WriteLine(a[0] + a[1] == a[2] ? "+" : "-");
+}
