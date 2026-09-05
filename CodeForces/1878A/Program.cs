b0538f8dff5391d40acfc01387397f1902b57afa
ELgago73 <gandjergholi@gmail.com>
2026-09-05 13:55:09 +0400

    Code Forces problem 1878A

---
+++ CodeForces/1878A/Program.cs
@@ -0,0 +1,19 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n, k;
+int[] cnt = new int[105];
+int[] a = new int[105];
+while(t-- > 0){
+    Array.Clear(cnt, 0, cnt.Length);
+    string[] input = Console.ReadLine().Split(' ');
+    n = int.Parse(input[0]);
+    k = int.Parse(input[1]);
+    a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    for(int i = 0; i < n; i++){
+        cnt[a[i]]++;
+    }
+    if(cnt[k] > 0){
+        Console.WriteLine("YES");
+    } else {
+        Console.WriteLine("NO");
+    }
+}
