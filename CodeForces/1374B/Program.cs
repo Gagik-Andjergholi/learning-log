06a4eba1c52acca0fddf02e34fd37b155872fdad
ELgago73 <gandjergholi@gmail.com>
2026-09-07 03:51:22 +0400

    Code Forces problem 1374B

---
+++ CodeForces/1374B/Program.cs
@@ -0,0 +1,23 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n, two, three;
+while(t-- > 0){
+    n = int.Parse(Console.ReadLine());
+    (two, three) = (0, 0);
+    while(n % 2 == 0)
+    {
+        n /= 2;
+        two++;
+    }
+    while(n % 3 == 0)
+    {
+        n /= 3;
+        three++;
+    }
+    if(n != 1 || two > three)
+    {
+        System.Console.WriteLine(-1);
+        continue;
+    }
+    System.Console.WriteLine(three * 2 - two);
+
+}
