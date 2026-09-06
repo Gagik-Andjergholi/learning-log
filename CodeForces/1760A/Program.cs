b0e5812dfbac0930e1ddbb50e341da9d17d24521
ELgago73 <gandjergholi@gmail.com>
2026-09-07 02:51:30 +0400

    Code Forces problem 1760A

---
+++ CodeForces/1760A/Program.cs
@@ -0,0 +1,6 @@
+﻿int t = int.Parse(Console.ReadLine());
+while(t-- > 0){
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    Array.Sort(a);
+    System.Console.WriteLine(a[1]);
+}
