8e954026a362fd45d780c7755e3ad4ae85798bb3
ELgago73 <gandjergholi@gmail.com>
2026-09-07 02:45:01 +0400

    Code Forces problem 1915A

---
+++ CodeForces/1915A/Program.cs
@@ -0,0 +1,22 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n, k;
+while(t-- > 0){
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    if(a[0] == a[1])
+    {
+        System.Console.WriteLine(a[2]);
+    }
+    else
+    {
+        if(a[0] == a[2])
+        {
+            System.Console.WriteLine(a[1]);
+            continue;
+        }
+        else
+        {
+            System.Console.WriteLine(a[0]);
+            continue;
+        }
+    }
+}
