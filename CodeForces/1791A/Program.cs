3ff39713a508f0a272881375d1072a5a7e0d24d5
ELgago73 <gandjergholi@gmail.com>
2026-09-07 01:31:22 +0400

    Code Forces problem 1791A

---
+++ CodeForces/1791A/Program.cs
@@ -0,0 +1,14 @@
+﻿int t = int.Parse(Console.ReadLine());
+string c = "codeforces";
+while(t-- > 0)
+{
+    string s = Console.ReadLine();
+    if (c.Contains(s))
+    {
+        System.Console.WriteLine("YES");
+    }
+    else
+    {
+        System.Console.WriteLine("NO");
+    }
+}
