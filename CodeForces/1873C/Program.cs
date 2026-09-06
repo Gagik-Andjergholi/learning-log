340a9d94fd199360e660c4ebbb367d5541f3723f
ELgago73 <gandjergholi@gmail.com>
2026-09-07 03:44:20 +0400

    Code Forces problem 1873C

---
+++ CodeForces/1873C/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+int t = int.Parse(Console.ReadLine());
+string s;
+int ans;
+while(t-- > 0){
+    ans = 0;
+    for(int i = 0; i < 10; i++)
+    {
+        s = Console.ReadLine();
+        for(int j = 0; j < 10; j++)
+        {
+            if(s[j] == 'X')
+            {
+                ans += calc(i, j);
+            }
+        }
+    }
+    System.Console.WriteLine(ans);
+}
+
+int calc(int x, int y)
+{
+    return Math.Min(Math.Min(x + 1, 10 - x), Math.Min(y + 1, 10 - y));
+}
