7e0cc1b15680e6eec6d7b3d31f90d2f1e48e9925
ELgago73 <gandjergholi@gmail.com>
2026-09-07 03:01:54 +0400

    Code Forces problem 32B

---
+++ CodeForces/32B/Program.cs
@@ -0,0 +1,19 @@
+﻿string s = Console.ReadLine();
+string ans = "";
+for(int  i = 0; i < s.Length; i++)
+{
+    if(s[i] == '.')
+    {
+        ans += "0";
+        continue;
+    }
+    if(s[i + 1] == '-')
+    {
+        ans += "2";
+        i++;
+        continue;
+    }
+    ans += "1";
+    i++;
+}
+System.Console.WriteLine(ans);
