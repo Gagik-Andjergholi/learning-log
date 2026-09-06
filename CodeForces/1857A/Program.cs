6b88a2a78b692e3f4950b19b24b6bf78e31e925c
ELgago73 <gandjergholi@gmail.com>
2026-09-06 14:03:30 +0400

    Code Forces problem 1857A

---
+++ CodeForces/1857A/Program.cs
@@ -0,0 +1,11 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n, o;
+while(t-- > 0)
+{
+    n = int.Parse(Console.ReadLine());
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    o = 0;
+    foreach(var x in a)
+        o += x % 2;
+    System.Console.WriteLine(o % 2 == 0 ? "YES" : "NO");
+}
