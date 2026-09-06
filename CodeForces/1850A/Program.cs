2e904b325539d57a883af1301798282c18d332a0
ELgago73 <gandjergholi@gmail.com>
2026-09-07 03:54:56 +0400

    Code Forces problem 1850A

---
+++ CodeForces/1850A/Program.cs
@@ -0,0 +1,7 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n;
+while(t-- > 0){
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    Array.Sort(a);
+    System.Console.WriteLine(a[1] + a[2] >= 10 ? "YES" : "NO");
+}
