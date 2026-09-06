63f4a63ee9522b3b8ebc91bc20d3391a8560dbd0
ELgago73 <gandjergholi@gmail.com>
2026-09-07 01:36:21 +0400

    Code Forces problem 1399A

---
+++ CodeForces/1399A/Program.cs
@@ -0,0 +1,16 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n;
+while(t-- > 0){
+    string s = "YES";
+    n = int.Parse(Console.ReadLine());
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    Array.Sort(a);
+    for(int i = 0; i < n - 1; i++)
+    {
+        if(a[i + 1] - a[i] > 1)
+        {
+            s = "NO";
+        }
+    }
+    System.Console.WriteLine(s);
+}
