31669f6161a1c0b495af1fcecab976c7d3901b4a
ELgago73 <gandjergholi@gmail.com>
2026-09-07 03:11:26 +0400

    Code Forces problem 1791C

---
+++ CodeForces/1791C/Program.cs
@@ -0,0 +1,13 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n, l, r;
+string s;
+while(t-- > 0){
+    n = int.Parse(Console.ReadLine());
+    s = Console.ReadLine();
+    l = 0; r = n - 1;
+    while(l < r && s[l] != s[r])
+    {
+        l++; r--; continue;
+    }
+    System.Console.WriteLine(r - l + 1);
+}
