86af7136c11d4654a2de6a191d3830b0a4341426
ELgago73 <gandjergholi@gmail.com>
2026-09-07 02:54:03 +0400

    Code Forces problem 1409A

---
+++ CodeForces/1409A/Program.cs
@@ -0,0 +1,5 @@
+﻿int t = int.Parse(Console.ReadLine());
+while(t-- > 0){
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    System.Console.WriteLine((Math.Abs(a[0]- a[1]) + 9) / 10);
+}
