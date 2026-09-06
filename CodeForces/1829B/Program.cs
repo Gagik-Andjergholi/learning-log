1ff56558685d160141f4c542e326c734431e7113
ELgago73 <gandjergholi@gmail.com>
2026-09-06 14:27:06 +0400

    Code Forces problem 1829B

---
+++ CodeForces/1829B/Program.cs
@@ -0,0 +1,19 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n, ans, cnt;
+while(t-- > 0)
+{
+    n = int.Parse(Console.ReadLine());
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    ans = 0; cnt = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] == 0)
+        {
+            cnt++;
+            ans = Math.Max(ans, cnt);
+            continue;
+        }
+        cnt = 0;
+    }
+    System.Console.WriteLine(ans);
+}
