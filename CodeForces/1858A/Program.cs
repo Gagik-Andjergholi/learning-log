b71f6f45768eafe7ec37475745173b98efd16524
ELgago73 <gandjergholi@gmail.com>
2026-09-07 04:22:45 +0400

    Code Forces problem 1858A

---
+++ CodeForces/1858A/Program.cs
@@ -0,0 +1,5 @@
+﻿int t = int.Parse(Console.ReadLine());
+while(t-- > 0){
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    System.Console.WriteLine((a[2] + 1) / 2 + a[0] > a[2] / 2 + a[1] ? "First" : "Second");
+}
