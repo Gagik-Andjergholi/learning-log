da6aabeba1790571933aeb8a42f1fa3d070d5634
ELgago73 <gandjergholi@gmail.com>
2026-09-08 23:11:18 +0400

    Code Forces problem 1829A

---
+++ CodeForces/1829A/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string target = "codeforces";
+        int t = int.Parse(Console.ReadLine());
+        while (t-- > 0)
+        {
+            string s = Console.ReadLine();
+            int count = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (s[i] != target[i]) count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
