7d3941151248e335559fa036f99cf77509cc671c
ELgago73 <gandjergholi@gmail.com>
2026-09-09 22:32:46 +0400

    Code Forces problem 1472B

---
+++ CodeForces/1472B/Program.cs
@@ -0,0 +1,18 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n, sum, one;
+while(t-- > 0){
+    sum = 0; one = 0;
+    n = int.Parse(Console.ReadLine());
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    for(int i = 0; i < n; i++)
+    {
+        sum += a[i];
+        one += a[i] % 2;
+    }
+    if(sum % 4 == 0 || (sum % 4 == 2 && one >= 2))
+    {
+        System.Console.WriteLine("YES");
+        continue;
+    }
+    System.Console.WriteLine("NO");
+}
