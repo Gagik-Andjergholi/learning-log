01968a4dc02fa9ca1dd456f4b4f4f31f8a9a17ad
ELgago73 <gandjergholi@gmail.com>
2026-09-12 18:57:52 +0400

    Code Forces problem 432A

---
+++ CodeForces/432A/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] trash = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int n = trash[0], k = trash[1];
+int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+Array.Sort(a);
+int ans = 0, t = 0;
+while(t + 2 <= n - 1)
+{
+    if(a[t++] <= 5 - k && a[t++] <= 5 - k && a[t++] <= 5 - k)
+    {
+        ans++;
+    }
+    else
+    {
+        break;
+    }
+}
+System.Console.WriteLine(ans);
