f1c96b8a0af9c45ad3d4df627de7dfc6085e3201
ELgago73 <gandjergholi@gmail.com>
2026-09-13 22:23:22 +0400

    Code Forces problem 1560A

---
+++ CodeForces/1560A/Program.cs
@@ -0,0 +1,16 @@
+﻿int t = int.Parse(Console.ReadLine());
+List<int> a = new List<int> {};
+for(int i = 1; i < 5000; i++)
+{
+    if(i % 3 == 0 || i % 10 == 3)
+    {
+        continue;
+    }
+    a.Add(i);
+}
+int n;
+while(t-- > 0)
+{
+    n = int.Parse(Console.ReadLine());
+    System.Console.WriteLine(a[n - 1]);
+}
