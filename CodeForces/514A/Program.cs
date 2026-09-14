24fa09c76fe8ad5310590432cfb793e882327973
ELgago73 <gandjergholi@gmail.com>
2026-09-14 17:04:26 +0400

    Code Forces problem 514A

---
+++ CodeForces/514A/Program.cs
@@ -0,0 +1,12 @@
+﻿string s = Console.ReadLine();
+s = s.Replace('9', '0');
+s = s.Replace('8', '1');
+s = s.Replace('7', '2');
+s = s.Replace('6', '3');
+s = s.Replace('5', '4');
+if (s.StartsWith('0'))
+{
+    s = s.Substring(1, s.Length - 1);
+    s = '9' + s;
+}
+System.Console.WriteLine(s);
