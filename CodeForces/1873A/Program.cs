19715ce1f9787ade2a246cf4d74a26ff9c0f1455
ELgago73 <gandjergholi@gmail.com>
2026-09-15 22:43:28 +0400

    Code Forces problem 1873A

---
+++ CodeForces/1873A/Program.cs
@@ -0,0 +1,14 @@
+﻿int t = int.Parse(Console.ReadLine());
+string s;
+while(t-- > 0)
+{
+    s = Console.ReadLine();
+    if(s[0] == 'a' || s[1] == 'b' || s[2] == 'c')
+    {
+        System.Console.WriteLine("YES");
+    }
+    else
+    {
+        System.Console.WriteLine("NO");
+    }
+}
