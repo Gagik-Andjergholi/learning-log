2654274b42c9d005a544f612e2ace279e3ad2ab1
ELgago73 <gandjergholi@gmail.com>
2026-09-15 22:50:40 +0400

    Code Forces problem 9A

---
+++ CodeForces/9A/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int x = 7 - Math.Max(a[0], a[1]);
+int six = 6;
+if(x % 2 == six % 2)
+{
+    x /= 2;
+    six /= 2;
+}
+if(x % 3 == six % 3)
+{
+    x /= 3;
+    six /= 3;
+}
+System.Console.WriteLine($"{x}/{six}");
