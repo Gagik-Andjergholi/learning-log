866c9aa19d33203249deeb6198581ab8efc0aed6
ELgago73 <gandjergholi@gmail.com>
2026-09-16 15:58:11 +0400

    Code Forces problem 472A

---
+++ CodeForces/472A/Program.cs
@@ -0,0 +1,21 @@
+﻿bool[] c = new bool[1000050];
+int n = 1000050;
+for(int i = 2; i < n; i++)
+{
+    if (!c[i])
+    {
+        for(int j = 2 * i; j < n; j += i)
+        {
+            c[j] = true;
+        }
+    }
+}
+n = int.Parse(Console.ReadLine());
+for(int i = 2; i < n; i++)
+{
+    if(c[i] && c[n - i])
+    {
+        System.Console.WriteLine(i + " " + (n - i));
+        break;
+    }
+}
