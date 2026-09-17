1543b9edbcee597e2e0c9771788d1b0fd5d2feb1
ELgago73 <gandjergholi@gmail.com>
2026-09-17 18:00:17 +0400

    Code Forces problem 43A

---
+++ CodeForces/43A/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = int.Parse(Console.ReadLine());
+string[] s = new string[n];
+for(int i = 0; i < n; i++)
+{
+    s[i] = Console.ReadLine();
+}
+Array.Sort(s);
+System.Console.WriteLine(s[n / 2]);
