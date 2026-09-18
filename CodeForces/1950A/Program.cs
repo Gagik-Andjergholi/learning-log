df1df234ebef5fc7034469b948065edb0ee9810a
ELgago73 <gandjergholi@gmail.com>
2026-09-18 23:22:25 +0400

    Code Froces problem 1950A

---
+++ CodeForces/1950A/Program.cs
@@ -0,0 +1,16 @@
+﻿int t = int.Parse(Console.ReadLine());
+while(t-- > 0)
+{
+    int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    if(a[0] < a[1] && a[1] < a[2])
+    {
+        System.Console.WriteLine("STAIR");
+        continue;
+    }
+    if(a[0] < a[1] && a[1] > a[2])
+    {
+        System.Console.WriteLine("PEAK");
+        continue;
+    }
+    System.Console.WriteLine("NONE");
+}
