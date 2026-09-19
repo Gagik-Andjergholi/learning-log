7340f47621278e49cfb444f5b87e935b60ce983f
ELgago73 <gandjergholi@gmail.com>
2026-09-19 22:21:09 +0400

    Code Forces problem 1703B

---
+++ CodeForces/1703B/Program.cs
@@ -0,0 +1,22 @@
+﻿int t = int.Parse(Console.ReadLine());
+int n, ans;
+string s;
+int[] b = new int[26];
+while(t-- > 0){
+    n = int.Parse(Console.ReadLine());
+    s = Console.ReadLine();
+    for(int i = 0; i < 26; i++)
+    {
+        b[i] = 0;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        b[s[i] - 'A']++;
+    }
+    ans = 0;
+    for(int i = 0; i < 26; i++)
+    {
+        ans += (b[i] == 0 ? 0 : b[i] + 1);
+    }
+    System.Console.WriteLine(ans);
+}
