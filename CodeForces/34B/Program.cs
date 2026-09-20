e5df4656b9d60207621a99407175886d0965b6eb
ELgago73 <gandjergholi@gmail.com>
2026-09-20 19:50:26 +0400

    Code Forces problem 34B

---
+++ CodeForces/34B/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] trash = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int ans = 0;
+int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+Array.Sort(a);
+for(int i = 0; i < trash[1]; i++)
+{
+    if(a[i] >= 0)
+    {
+        break;
+    }
+    ans += a[i];
+}
+System.Console.WriteLine(-1 * ans);
