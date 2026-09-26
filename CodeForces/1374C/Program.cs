f9cbb4ea59afb9eccb0218be0d78fed556f30b44
ELgago73 <gandjergholi@gmail.com>
2026-09-26 20:02:51 +0400

    Code Forces problem 1374C

---
+++ CodeForces/1374C/Program.cs
@@ -0,0 +1,19 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+while (t-- > 0) {
+    int n = int.Parse(Console.ReadLine());
+    string s = Console.ReadLine();
+
+    int balance = 0, ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '(')
+            balance++;
+        else if (balance > 0)
+            balance--;
+        else
+            ans++;
+    }
+
+    Console.WriteLine(ans);
+}
